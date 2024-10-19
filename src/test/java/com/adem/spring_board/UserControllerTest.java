90722521ef9e350e639cd22eb715c6b4b77aeaf9
Jugal Kishor Sahu <jugalkishorsahu5@gmail.com>
2024-10-19 22:13:43 +0530

    spring-board, test

--- src/test/java/com/adem/spring_board/UserControllerTest.java
+++ src/test/java/com/adem/spring_board/UserControllerTest.java
@@ -4,8 +4,9 @@ import com.adem.spring_board.dto.AuthRequest;
 import com.adem.spring_board.model.User;
 import com.adem.spring_board.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-//@Sql(scripts = "/schema.sql")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserControllerTest {
 
     @Autowired private MockMvc mockMvc;
@@ -35,19 +36,19 @@ public class UserControllerTest {
     @Autowired private PasswordEncoder passwordEncoder;
 
 
-    String SCHEMA = "-- Table structure for table `users`\n" + "DROP TABLE IF EXISTS users;\n" + "\n" + "CREATE TABLE users (\n" + "  id BIGINT NOT NULL AUTO_INCREMENT,\n" + "  email VARCHAR(255) DEFAULT NULL,\n" + "  username VARCHAR(255) DEFAULT NULL,\n" + "  address VARCHAR(255) DEFAULT NULL,\n" + "  name VARCHAR(255) DEFAULT NULL,\n" + "  type TINYINT DEFAULT NULL,\n" + "  password VARCHAR(255) DEFAULT NULL,\n" + "  roles VARCHAR(255) DEFAULT NULL,\n" + "  PRIMARY KEY (id)\n" + ");\n" + "\n" + "-- Table structure for table `product`\n" + "DROP TABLE IF EXISTS product;\n" + "\n" + "CREATE TABLE product (\n" + "  id BIGINT NOT NULL AUTO_INCREMENT,\n" + "  name VARCHAR(255) DEFAULT NULL,\n" + "  price DOUBLE DEFAULT NULL,\n" + "  quantity INT DEFAULT NULL,\n" + "  PRIMARY KEY (id)\n" + ");\n" + "\n" + "-- Table structure for table `orders`\n" + "DROP TABLE IF EXISTS orders;\n" + "\n" + "CREATE TABLE orders (\n" + "  id BIGINT NOT NULL AUTO_INCREMENT,\n" + "  quantity INT NOT NULL,\n" + "  product_id BIGINT DEFAULT NULL,\n" + "  user_id BIGINT DEFAULT NULL,\n" + "  PRIMARY KEY (id),\n" + "  FOREIGN KEY (user_id) REFERENCES users(id),\n" + "  FOREIGN KEY (product_id) REFERENCES product(id)\n" + ");\n" + "\n" + "-- Dumping data for table `users`\n";
+    String SCHEMA = "CREATE TABLE users ( " + "id BIGINT NOT NULL AUTO_INCREMENT, " + "email VARCHAR(255), " + "username VARCHAR(255), " + "name VARCHAR(255), " + "password VARCHAR(255), " + "roles VARCHAR(255), " + "PRIMARY KEY (id) " + ");";
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         jdbcTemplate.execute(SCHEMA);
 
-        userRepository.save(new User(null, "username", passwordEncoder.encode("password"), "usedr"));
+        userRepository.save(new User(null, "testuser01", passwordEncoder.encode("password"), "user"));
     }
 
     @Test
     void testRegisterUser() throws Exception {
         User user = new User();
-        user.setUsername("testuser");
+        user.setUsername("testuser02");
         user.setPassword("password");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/users/register").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(user))).andExpect(status().isOk());
@@ -58,7 +59,7 @@ public class UserControllerTest {
         // Assume user is already registered
 
         AuthRequest authRequest = new AuthRequest();
-        authRequest.setUsername("testuser");
+        authRequest.setUsername("testuser01");
         authRequest.setPassword("password");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/users/login").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(authRequest))).andExpect(status().isOk());
